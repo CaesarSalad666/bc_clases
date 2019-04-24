60da1e07f3c68a96f0ea30ae2b24a0440ef4996b
César Arroyo <cesararroyolezcano@gmail.com>
2019-04-24 16:22:48 -0400

    Ejemplo de clases

---
+++ dino.py
@@ -0,0 +1,19 @@
+
+class Dino:
+    patas = 4
+    nombre = None
+    def __init__(self, canti_patas, un_nombre):
+        self.patas = canti_patas
+        self.nombre = un_nombre
+        print("Hola soy un dinosaurio","me llamo", self.nombre, " y tengo", self.patas, "patas.")
+    
+    def get_patas(self):
+        return self.patas
+
+    def set_patas(self, cantidad):
+        self.patas = cantidad
+
+pepito = Dino(4, "Pepito")
+
+#En el archivo persona.py, crear una clase persona con atributo nombre
+#despues instanciar un objeto de tipo persona
