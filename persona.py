a49e1543ee60c4fe999015417e247cf1338e2aeb
CaesarSalad666 <49949865+CaesarSalad666@users.noreply.github.com>
2019-04-24 15:20:59 -0400

    Ultima modificacion

--- persona.py
+++ persona.py
@@ -1,11 +1,5 @@
 #Esto es una prueba
 print("Que tal Mundo?")
-<<<<<<< HEAD
 print("a little something")
 print("ALGO DOS")
 print("Algo3")
-=======
-print("xxxxx")
-print("a little something 0")
-print("ALGO DOS")
->>>>>>> 1939519c7c0d849dbaf43da246fd129e1bcfe572
